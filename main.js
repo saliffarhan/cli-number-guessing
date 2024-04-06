13b21b65eab598432c8ad2501a2015ba6f6c70d5
saliffarhan <161368049+saliffarhan@users.noreply.github.com>
2024-04-07 03:18:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+        name: "num1",
+        type: "number",
+        message: "Enter your value 1 to 10 : ",
+    }]);
+if (answer.num1 === randomNumber) {
+    console.log("Congratulations You Guess the right number ");
+}
+else {
+    console.log("Sorry you guess the Wrong Number ");
+}
+;
